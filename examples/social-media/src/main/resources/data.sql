bd5c2c4af362e23947731d81ebcdf2a16a821293
shg@dsi <akter.hossain@dsinnovators.com>
2018-01-01 15:41:42 +0600

    sample data

---
+++ examples/social-media/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO author (id, first_name, last_Name) VALUES
+    (1, 'Akter', 'Hossain');
+
+INSERT INTO post (id, title, body, author_id) VALUES
+    (1, 'Beautiful Bangladesh!', 'Welcome to the one of the most beautiful country of the world.', 1),
+    (2, 'Delighting Dhaka!', 'Dhaka is not only mostly dense city in the world, it has lot of delightful history.', 1);
